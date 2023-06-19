f13c14064946c7401673335d6607f2475328f32a
demianjun <demianjun@gmail.com>
2023-06-19 15:38:15 +0900

    init func name

--- DeepmediFingerKit/Classes/Camera/CameraPreview.swift
+++ DeepmediFingerKit/Classes/Camera/CameraPreview.swift
@@ -8,7 +8,7 @@
 import Foundation
 import AVKit
 
-public class FingerCameraPreview: UIView {
+public class CameraPreview: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
